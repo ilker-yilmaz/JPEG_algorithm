363fcea1bf0603460282de5327d292704ef02744
ilker yılmaz <53461108+ilkerylmaz@users.noreply.github.com>
2021-04-06 13:01:39 +0300

    commit-2  6 nisan 2021

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+ 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+
+
 export class HomeComponent implements OnInit {
 
   constructor() { }
@@ -14,3 +17,4 @@ export class HomeComponent implements OnInit {
   }
 
 }
+ 
