dffc58d53f63b95aafd29c233e1ebd9be97b2c98
ilker yılmaz <53461108+ilkerylmaz@users.noreply.github.com>
2021-05-20 21:19:46 +0300

    auth0 configuration FİX-2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   auth:{
-    "AUTH0_DOMAIN":"ilker-blog.us.auth0.com",
-  "AUTH0_CLIENT_ID":"LzyqXLpMqz35iZXhNLWtXnWieVjKerJD"
+    AUTH0_DOMAIN:"ilker-blog.us.auth0.com",
+  AUTH0_CLIENT_I:"LzyqXLpMqz35iZXhNLWtXnWieVjKerJD"
   }
 };
